5b4fa2781946a53bda9144455f7145f07ff79162
Eric Ha <eric_ha@outlook.com>
2017-12-06 19:16:33 +0100

    update title
    update title

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (!$errName && !$errEmail && !$errMessage) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Herr's Job</title>
+    <title>Her's Job</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-select.css">
   </head>
